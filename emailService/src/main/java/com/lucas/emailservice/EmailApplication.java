24305d260b41f8251342580e8e36442dc11cf090
lucasfen <lucasfen@etekcity.com.cn>
2021-01-08 15:12:16 +0800

    springboot初始化

---
+++ emailService/src/main/java/com/lucas/emailservice/EmailApplication.java
@@ -0,0 +1,16 @@
+package com.lucas.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * @author: lucasfen
+ * @create: 2021/01/08
+ */
+@SpringBootConfiguration
+public class EmailApplication {
+
+    public static void main(String args[]) {
+        SpringApplication.run(EmailApplication.class, args);
+    }
+}
